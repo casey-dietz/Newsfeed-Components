c98411a23ba0de361917e8869a6eb2686c6c4740
Casey Dietz <casey.dietz@ihsmarkit.com>
2021-01-19 18:37:13 -0700

    MVP complete

--- components/Menu.js
+++ components/Menu.js
@@ -31,3 +31,29 @@ let menuItems = [
 
   Step 6: Use 'menuMaker' to create a menu using the 'menuItems' array, and append the returned menu to the header.
 */
+
+function menuMaker(arr){
+  const menu = document.createElement('div')
+  menu.classList.add('menu')
+
+  const uList = document.createElement('ul')
+  menu.appendChild(uList)
+
+  menuItems.forEach((item) => {
+    const list = document.createElement('li')
+    list.textContent = item
+    uList.appendChild(list)
+  })
+
+  const menuButton = document.querySelector('.menu-button')
+  menuButton.addEventListener('click', () => {
+    menu.classList.toggle('menu--open')
+  })
+
+  return menu
+}
+
+const header = document.querySelector('.header')
+
+header.appendChild(menuMaker(menuItems))
+
